f7996393c5c63926958b118a73010a8cd8a22bbb
ITKaty <117355327+chabanova-ekaterina@users.noreply.github.com>
2023-03-07 16:47:10 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -67,11 +67,3 @@ const navSlide = () => {
   };
   
   navSlide();
-
-/*counter*/
-const button = document.querySelector('.button');
-let count;
-
-button.addEventListener('click', () => {
-  count =+ 1;
-});
